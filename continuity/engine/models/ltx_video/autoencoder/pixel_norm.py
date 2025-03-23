bed4502cceb939b23d56ee213d79abea22ae12b7
shauray8 <shauray9@gmail.com>
2025-03-23 22:32:49 +0530

    rough autoencoder

---
+++ continuity/engine/models/ltx_video/autoencoder/pixel_norm.py
@@ -0,0 +1,13 @@
+#copied from https://github.com/Lightricks/LTX-Video/blob/main/ltx_video/models/autoencoders/pixel_norm.py
+import torch
+from torch import nn
+
+
+class PixelNorm(nn.Module):
+    def __init__(self, dim=1, eps=1e-8):
+        super(PixelNorm, self).__init__()
+        self.dim = dim
+        self.eps = eps
+
+    def forward(self, x):
+        return x / torch.sqrt(torch.mean(x**2, dim=self.dim, keepdim=True) + self.eps)
