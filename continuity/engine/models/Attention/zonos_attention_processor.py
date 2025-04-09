3156f9086666daf9748565412724d392618c24ba
shauray8 <shauray9@gmail.com>
2025-04-10 01:02:10 +0530

    move kv cache to attn

--- continuity/engine/models/Attention/zonos_attention_processor.py
+++ continuity/engine/models/Attention/zonos_attention_processor.py
@@ -5,18 +5,28 @@ from torch.nn import functional as F
 def apply_rotary_emb(x: torch.Tensor, freqs_cis: torch.Tensor) -> torch.Tensor:
     xshaped = x.float().reshape(*x.shape[:-1], -1, 2)
     freqs_cis = freqs_cis.view(-1, xshaped.size(1), 1, xshaped.size(3), 2)
-    x_out2 = torch.stack(
-        [
-            xshaped[..., 0] * freqs_cis[..., 0] - xshaped[..., 1] * freqs_cis[..., 1],
-            xshaped[..., 1] * freqs_cis[..., 0] + xshaped[..., 0] * freqs_cis[..., 1],
-        ],
-        -1,
-    )
-
+    x_out2 = torch.stack([xshaped[..., 0] * freqs_cis[..., 0] - xshaped[..., 1] * freqs_cis[..., 1], xshaped[..., 1] * freqs_cis[..., 0] + xshaped[..., 0] * freqs_cis[..., 1],],-1,)
     x_out2 = x_out2.flatten(3)
     return x_out2.type_as(x)
 
-class Attention(nn.Module):
+def _update_kv_cache(
+    k: torch.Tensor, v: torch.Tensor, inference_params: InferenceParams, layer_idx: int
+) -> torch.Tensor:
+    """k/v: (batch_size, seqlen, nheads, head_dim) or (batch_size, 1, nheads, head_dim)"""
+    assert layer_idx in inference_params.key_value_memory_dict
+    kv_cache, _ = inference_params.key_value_memory_dict[layer_idx]
+    batch_start = inference_params.batch_size_offset
+    batch_end = batch_start + k.shape[0]
+    sequence_start = inference_params.seqlen_offset
+    sequence_end = sequence_start + k.shape[1]
+    assert batch_end <= kv_cache.shape[0]
+    assert sequence_end <= kv_cache.shape[1]
+    assert kv_cache is not None
+    kv_cache[batch_start:batch_end, sequence_start:sequence_end, 0, ...] = k
+    kv_cache[batch_start:batch_end, sequence_start:sequence_end, 1, ...] = v
+    return kv_cache[batch_start:batch_end, :sequence_end, ...]
+
+class ZonosAttentionBlock(nn.Module):
     def __init__(self, config: BackboneConfig, layer_idx: int):
         super().__init__()
         self.num_heads = config.attn_cfg["num_heads"]
@@ -44,12 +54,8 @@ class Attention(nn.Module):
 
         kv = _update_kv_cache(k, v, inference_params, self.layer_idx)
         k, v = kv.unbind(dim=-3)
-
         q, k, v = map(lambda x: x.transpose(1, 2), (q, k, v))
-
         y = F.scaled_dot_product_attention(q, k, v, is_causal=seqlen > 1, enable_gqa=True)
-
         y = y.transpose(1, 2).contiguous().view(batch_size, seqlen, q_size)
-
         y = self.out_proj(y)
         return y
